6078310543cc4d343c645a3c806fd25bbe419dbc
Rameshkumar Kamalanathan <kramesh21@gmail.com>
2016-04-08 19:01:23 -0700

    commited by Ramesh

---
+++ run.bat
@@ -0,0 +1,6 @@
+
+SET PATH=d:\Java\ant\ant\apache-ant-1.6.5\bin;C:\Program Files\Java\jdk1.5.0_03\bin;D:\Java\jboss-4.0.3SP1\bin;%PATH%;
+SET JAVA_HOME=C:\Program Files\Java\jdk1.5.0_03
+SET JBOSS_HOME=D:\Java\jboss-4.0.3SP1
+ant deploy-ear
+
